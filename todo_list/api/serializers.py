7b7ec829e4a7226e181d0ab96ea05206ae7109c2
Тимофей Баев <timofey@Timofejs-MacBook-Pro.local>
2023-11-26 20:27:13 +0400

    added put and delete methods

--- todo_list/api/serializers.py
+++ todo_list/api/serializers.py
@@ -3,19 +3,7 @@ from .models import ToDo
 from rest_framework import serializers
 
 
-class ToDoCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ToDo
-        fields = ('id',)
-
-
-class ToDoListSerializer(serializers.ModelSerializer):
+class ToDoModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = ToDo
         fields = '__all__'
-
-
-class ToDoDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ToDo
-        fields = ('id', 'title', 'description')
--- todo_list/api/views.py
+++ todo_list/api/views.py
@@ -1,10 +1,10 @@
-from .serializers import (ToDoCreateSerializer,
-                          ToDoListSerializer,
-                          ToDoDetailSerializer)
-from .models import ToDo
-
+from django.http import Http404 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import ToDoModelSerializer
+from .models import ToDo
 
 
 class ToDoCreateListView(APIView):
@@ -12,22 +12,43 @@ class ToDoCreateListView(APIView):
     
     def get(self, request):
         tasks = ToDo.objects.all()
-        serializer = ToDoListSerializer(tasks, many=True)
+        serializer = ToDoModelSerializer(tasks, many=True)
         return Response(serializer.data)
     
     def post(self, request):
-        new_task = ToDo.objects.create(
-            title=request.data['title'],
-            description=request.data['description']
-        )
-        serializer = ToDoCreateSerializer(new_task)
-        return Response(serializer.data)
+        serializer = ToDoModelSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response({'id': serializer.data['id']},
+                            status=status.HTTP_201_CREATED)
+            
+        return Response(serializer.errors,
+                        status=status.HTTP_404_NOT_FOUND)
 
 
 class ToDoDetailView(APIView):
     """ Detail task """
     
+    def get_object(self, pk):
+        try:
+            return ToDo.objects.get(pk=pk)
+        except ToDo.DoesNotExist:
+            return Http404
+    
     def get(self, request, pk):
-        task = ToDo.objects.get(id=pk)
-        serializer = ToDoDetailSerializer(task)
+        task = self.get_object(pk)
+        serializer = ToDoModelSerializer(task)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        task = self.get_object(pk)
+        serializer = ToDoModelSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    def delete(self, request, pk):
+        task = self.get_object(pk)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
